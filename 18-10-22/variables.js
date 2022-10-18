5ce3dfb95b6a45f0f7209822c2d8fc2fdae3be76
Steve <itsmesteve88@yahoo.com>
2022-10-18 14:28:46 +0100

    Daily work

---
+++ 18-10-22/variables.js
@@ -0,0 +1,35 @@
+// How to create variables in JavaScript
+// firstname, lname, dob, gender, address, phone.
+
+var firstname = "John";
+console.log(firstname);
+
+// What are keywords in JavaScript?
+// They are predfeined words in JavaScript that have special meaning.
+
+// var, console, function, if, for, while, let, const, break, function and return are keywords in JavaScript.
+
+// What are identifiers in JavaScript?
+// They are names given to variables, functions, objects, and properties.
+
+// What are datatypes in JavaScript?
+// They are the type of data that can be stored in a variable.
+
+// They are: string, number, boolean, null, undefined, object, symbol.
+
+// To find a datatype of a variable, use the typeof operator.
+
+// Object
+// Stores data in key-value pairs.
+
+var employee = { id: 1, name: "John", age: 35, salary: 1000 };
+console.log(employee);
+console.log(typeof employee);
+
+// Undefined
+// It is a datatype that indicates that a variable has not been assigned a value.
+
+var x;
+console.log(x);
+
+
